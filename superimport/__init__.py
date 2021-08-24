99b7561b909fc43375ea3367d0aac927298d2a8b
Mahmoud Soliman <mjsML@users.noreply.github.com>
2021-08-24 16:27:15 +0100

    3.7!

--- superimport/__init__.py
+++ superimport/__init__.py
@@ -1,5 +1,2 @@
 from superimport import superimport
-# import sys
-# system=sys.version_info
-# if system[0]==3 and system[1]>=9:
-#     from superimport.superimport import *
+from superimport.superimport import unimport
