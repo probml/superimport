db5f747332cfe463c236e4cbfd154b85ee5276f5
Mahmoud Soliman <mjsML@users.noreply.github.com>
2021-09-06 12:13:38 +0100

    testing

--- superimport/_version.py
+++ superimport/_version.py
@@ -1,7 +1,7 @@
 # from datetime import datetime
 # build_time=datetime.now().strftime('%Y%m%d%H%M%S')
 
-# __version__ = f"0.0.3.dev1.{build_time}"
+# __version__ = f"0.3.3.dev1.{build_time}"
 # print(__version__)
 
-__version__= "0.3.3"
+__version__= "0.3.3.dev1.20210906121309"
